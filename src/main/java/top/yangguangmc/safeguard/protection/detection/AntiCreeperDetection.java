267237774b8d5f3aad4b50ca0171a1908c3418c6
yangguangMC <fzzxq88888@163.com>
2026-07-09 15:08:59 +0800

    AntiCreeperDetection

--- src/main/java/top/yangguangmc/safeguard/protection/detection/AntiCreeperDetection.java
+++ src/main/java/top/yangguangmc/safeguard/protection/detection/AntiCreeperDetection.java
@@ -2,6 +2,7 @@ package top.yangguangmc.safeguard.protection.detection;
 
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
+import net.minecraft.client.render.Camera;
 import net.minecraft.client.world.ClientWorld;
 import net.minecraft.entity.Entity;
 import net.minecraft.entity.mob.CreeperEntity;
@@ -10,8 +11,14 @@ import net.minecraft.text.Style;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
+import net.minecraft.util.math.MathHelper;
+import net.minecraft.util.math.Vec3d;
+import net.minecraft.world.waypoint.EntityTickProgress;
 import top.yangguangmc.safeguard.ModContext;
-import top.yangguangmc.safeguard.protection.action.*;
+import top.yangguangmc.safeguard.protection.action.Action;
+import top.yangguangmc.safeguard.protection.action.PauseAction;
+import top.yangguangmc.safeguard.protection.action.PlaySoundAction;
+import top.yangguangmc.safeguard.protection.action.QuitAction;
 import top.yangguangmc.safeguard.protection.event.ClientPlayerTickEvents;
 
 import java.util.function.UnaryOperator;
@@ -50,14 +57,12 @@ public class AntiCreeperDetection extends Detection {
         if (e != null) {
             if (minD <= distance * distance) {
                 double d2 = Math.sqrt(minD);
-                if (e.getVelocity().lengthSquared() >= 0.0001) {
-                    ActionBarTitleAction action = getBoundAction(Identifier.of(ModContext.MOD_ID, "passive/hud/action_bar_title"));
-                    if (isActionEffectivelyEnabled(action)) action.updateTitle(client, d2, style -> {
-                        if (d2 <= 1 / 2.0 * distance) return style.withColor(Formatting.RED).withBold(true);
-                        else if (d2 <= 3 / 4.0 * distance) return style.withColor(Formatting.GOLD);
-                        else return style.withColor(Formatting.YELLOW);
-                    });
-                }
+                ActionBarTitleAction action = getBoundAction(Identifier.of(ModContext.MOD_ID, "passive/hud/action_bar_title"));
+                if (isActionEffectivelyEnabled(action)) action.updateTitle(client, world, e, d2, style -> {
+                    if (d2 <= 1 / 2.0 * distance) return style.withColor(Formatting.RED).withBold(true);
+                    else if (d2 <= 3 / 4.0 * distance) return style.withColor(Formatting.GOLD);
+                    else return style.withColor(Formatting.YELLOW);
+                });
                 PlaySoundAction action2 = getBoundAction(Identifier.of(ModContext.MOD_ID, "passive/other/play_sound"));
                 if (isActionEffectivelyEnabled(action2)) {
                     action2.tick(client);
@@ -78,8 +83,26 @@ public class AntiCreeperDetection extends Detection {
             super("passive/hud/action_bar_title");
         }
 
-        public void updateTitle(MinecraftClient client, double distance, UnaryOperator<Style> styleProvider) {
-            client.inGameHud.setOverlayMessage(Text.literal("警告：苦力怕距离你 %.1f 方块".formatted(distance)).styled(styleProvider), false);
+        public void updateTitle(MinecraftClient client, ClientWorld world, CreeperEntity creeper, double distance, UnaryOperator<Style> styleProvider) {
+            Camera camera = client.gameRenderer.getCamera();
+            double relativeYaw = getRelativeYaw(camera.getCameraPos(), camera.getCameraYaw(), creeper, e -> client.getRenderTickCounter().getTickProgress(!world.getTickManager().shouldSkipTick(e)));
+            String directionIndicator;
+            if (relativeYaw < -180 || relativeYaw > 180) throw new AssertionError();
+            if (relativeYaw >= -157.5 && relativeYaw < -112.5) directionIndicator = "↙";
+            else if (relativeYaw >= -112.5 && relativeYaw < -67.5) directionIndicator = "←";
+            else if (relativeYaw >= -67.5 && relativeYaw < -22.5) directionIndicator = "↖";
+            else if (relativeYaw >= -22.5 && relativeYaw < 22.5) directionIndicator = "↑";
+            else if (relativeYaw >= 22.5 && relativeYaw < 67.5) directionIndicator = "↗";
+            else if (relativeYaw >= 67.5 && relativeYaw < 112.5) directionIndicator = "→";
+            else if (relativeYaw >= 112.5 && relativeYaw < 157.5) directionIndicator = "↘";
+            else directionIndicator = "↓";  // relativeYaw <= -157.5 || relativeYaw >= 157.5
+            client.inGameHud.setOverlayMessage(Text.literal("警告：苦力怕距离你 %.1f 方块 %s".formatted(distance, directionIndicator)).styled(styleProvider), false);
+        }
+
+        private double getRelativeYaw(Vec3d cameraPos, float yaw, Entity entity, EntityTickProgress tickProgress) {
+            Vec3d vec3d = cameraPos.subtract(entity.getCameraPosVec(tickProgress.getTickProgress(entity))).rotateYClockwise();
+            float f = (float) MathHelper.atan2(vec3d.getZ(), vec3d.getX()) * (180.0F / (float) Math.PI);
+            return MathHelper.subtractAngles(yaw, f);
         }
     }
 }
