0e5b7ac327e18e864d14fe53ed4c5107f468fa62
yangguangMC <fzzxq88888@163.com>
2026-07-17 11:08:46 +0800

    Add action binding

--- src/main/java/top/yangguangmc/safeguard/protection/detection/AntiAmbushDetection.java
+++ src/main/java/top/yangguangmc/safeguard/protection/detection/AntiAmbushDetection.java
@@ -3,12 +3,18 @@ package top.yangguangmc.safeguard.protection.detection;
 import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.network.ClientPlayerEntity;
 import net.minecraft.client.world.ClientWorld;
+import net.minecraft.entity.Entity;
 import net.minecraft.entity.LivingEntity;
+import net.minecraft.entity.mob.GhastEntity;
+import net.minecraft.entity.mob.HostileEntity;
+import net.minecraft.entity.mob.PhantomEntity;
+import net.minecraft.entity.mob.SlimeEntity;
+import net.minecraft.entity.player.PlayerEntity;
 import net.minecraft.text.Text;
 import net.minecraft.util.Formatting;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.math.ColorHelper;
 import top.yangguangmc.safeguard.ModContext;
+import top.yangguangmc.safeguard.protection.action.Action;
 import top.yangguangmc.safeguard.protection.action.OutlineAction;
 import top.yangguangmc.safeguard.protection.event.ClientPlayerTickEvents;
 import top.yangguangmc.safeguard.util.Utils;
@@ -29,29 +35,49 @@ public class AntiAmbushDetection extends Detection {
         List<LivingEntity> entities = world.getOtherEntities(
                         player,
                         player.getBoundingBox().expand(16),
-                        entity -> entity instanceof LivingEntity && entity.isAlive()
+                        entity -> entity.isAlive() && (
+                                entity instanceof HostileEntity
+                                        || entity instanceof PlayerEntity
+                                        || entity instanceof SlimeEntity
+                                        || entity instanceof PhantomEntity
+                                        || entity instanceof GhastEntity
+                        )
                 ).stream()
                 .map(entity -> (LivingEntity) entity)
                 .filter(entity -> entity.age % checkInterval == 0)
                 .filter(entity -> player.squaredDistanceTo(entity) <= 16 * 16)
+                .filter(other -> !player.isTeammate(other))
                 .filter(entity -> (entity.isInvisible() && entity.isInvisibleTo(player)) || !player.canSee(entity))
                 .sorted(Comparator.comparing(player::squaredDistanceTo))
                 .toList();
         @SuppressWarnings("DataFlowIssue") final int color = Formatting.GOLD.getColorValue();
-        if (!entities.isEmpty())
+        if (!entities.isEmpty()) {
+            ActionBarTitleAction action = getBoundAction(Identifier.of(ModContext.MOD_ID, "passive/hud/action_bar_title"));
+            if (isActionEffectivelyEnabled(action))
+                action.updateTitle(client, world, entities.size(), entities.getFirst(), color);
+        }
+        for (LivingEntity entity : entities) {
+            OutlineAction action = getBoundAction(Identifier.of(ModContext.MOD_ID, "passive/other/outline"));
+            if (isActionEffectivelyEnabled(action)) action.outline(entity, 60, color);
+        }
+    }
+
+    private static class ActionBarTitleAction extends Action {
+        public ActionBarTitleAction() {
+            super("passive/hud/action_bar_title");
+        }
+
+        public void updateTitle(MinecraftClient client, ClientWorld world, int amount, Entity nearest, int color) {
             client.inGameHud.setOverlayMessage(
                     Text.literal("警告：发现潜在偷袭者，数量：")
-                            .append(String.valueOf(entities.size()))
+                            .append(String.valueOf(amount))
                             .append(" 最近者：")
-                            .append(entities.getFirst().getDisplayName())
+                            .append(nearest.getDisplayName())
                             .append(" ")
-                            .append(Utils.getDirectionIndicator(client, world, entities.getFirst(), client.gameRenderer.getCamera()))
+                            .append(Utils.getDirectionIndicator(client, world, nearest, client.gameRenderer.getCamera()))
                             .withColor(color),
                     false
             );
-        for (LivingEntity entity : entities) {
-            OutlineAction action = getBoundAction(Identifier.of(ModContext.MOD_ID, "passive/other/outline"));
-            action.outline(entity, 60, color);
         }
     }
 }
