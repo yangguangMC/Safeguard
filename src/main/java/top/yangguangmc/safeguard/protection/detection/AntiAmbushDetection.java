527699e642fc3d506e7ddeb294f76b1ba5db16ed
yangguangMC <fzzxq88888@163.com>
2026-07-30 22:50:13 +0800

    Fix: error in enemy count display

--- src/main/java/top/yangguangmc/safeguard/protection/detection/AntiAmbushDetection.java
+++ src/main/java/top/yangguangmc/safeguard/protection/detection/AntiAmbushDetection.java
@@ -21,13 +21,18 @@ import java.util.Comparator;
 import java.util.List;
 
 public class AntiAmbushDetection extends Detection {
+    @SuppressWarnings("FieldMayBeFinal")
+    private int checkInterval = 5;
+    private int tickCounter;
+
     public AntiAmbushDetection() {
         super("combat/anti_ambush", new ActionBarTitleAction(), new OutlineAction());
         listen(ClientPlayerTickEvents.GATED_START_TICK, this::onStartTick);
     }
 
     private void onStartTick(MinecraftClient client, ClientWorld world, ClientPlayerEntity player) {
-        final int checkInterval = 5;
+        tickCounter++;
+        if (tickCounter % checkInterval != 0) return;
         List<LivingEntity> entities = world.getOtherEntities(
                         player,
                         player.getBoundingBox().expand(16),
@@ -40,7 +45,6 @@ public class AntiAmbushDetection extends Detection {
                         )
                 ).stream()
                 .map(entity -> (LivingEntity) entity)
-                .filter(entity -> entity.age % checkInterval == 0)
                 .filter(entity -> player.squaredDistanceTo(entity) <= 16 * 16)
                 .filter(other -> !player.isTeammate(other))
                 .filter(entity -> (entity.isInvisible() && entity.isInvisibleTo(player)) || !player.canSee(entity))
@@ -64,9 +68,9 @@ public class AntiAmbushDetection extends Detection {
         public void updateTitle(MinecraftClient client, ClientWorld world, int amount, Entity nearest, int color) {
             client.inGameHud.setOverlayMessage(
                     Text.translatable("detection.safeguard.combat.anti_ambush.warning",
-                            String.valueOf(amount),
-                            nearest.getDisplayName(),
-                            Utils.getDirectionIndicator(client, world, nearest, client.gameRenderer.getCamera()))
+                                    String.valueOf(amount),
+                                    nearest.getDisplayName(),
+                                    Utils.getDirectionIndicator(client, world, nearest, client.gameRenderer.getCamera()))
                             .withColor(color),
                     false
             );
