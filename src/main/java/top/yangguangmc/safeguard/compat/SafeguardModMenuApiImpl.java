3bde4fe668da4d87ce1d7690b34605ef73972ce8
yangguangMC <fzzxq88888@163.com>
2026-08-01 13:11:21 +0800

    Refactor: move to package compact

--- src/main/java/top/yangguangmc/safeguard/compat/SafeguardModMenuApiImpl.java
+++ src/main/java/top/yangguangmc/safeguard/compat/SafeguardModMenuApiImpl.java
@@ -1,4 +1,4 @@
-package top.yangguangmc.safeguard;
+package top.yangguangmc.safeguard.compat;
 
 import com.terraformersmc.modmenu.api.ConfigScreenFactory;
 import com.terraformersmc.modmenu.api.ModMenuApi;
