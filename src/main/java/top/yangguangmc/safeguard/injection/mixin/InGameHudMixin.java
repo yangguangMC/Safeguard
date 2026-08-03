025c1b5992c73ff360e6e7f78269cb61e2c4d494
yangguangMC <fzzxq88888@163.com>
2026-08-03 17:54:04 +0800

    fix: prevent RedVignetteAction from affecting creative mode.

--- src/main/java/top/yangguangmc/safeguard/injection/mixin/InGameHudMixin.java
+++ src/main/java/top/yangguangmc/safeguard/injection/mixin/InGameHudMixin.java
@@ -1,16 +1,19 @@
 package top.yangguangmc.safeguard.injection.mixin;
 
+import net.minecraft.client.MinecraftClient;
 import net.minecraft.client.gui.hud.InGameHud;
 import net.minecraft.util.math.ColorHelper;
 import org.spongepowered.asm.mixin.Mixin;
 import org.spongepowered.asm.mixin.injection.At;
 import org.spongepowered.asm.mixin.injection.ModifyVariable;
+import top.yangguangmc.safeguard.protection.GlobalProtectionConditions;
 import top.yangguangmc.safeguard.protection.action.RedVignetteAction;
 
 @Mixin(InGameHud.class)
 public class InGameHudMixin {
     @ModifyVariable(method = "renderVignetteOverlay", at = @At(value = "INVOKE", target = "Lnet/minecraft/client/gui/DrawContext;drawTexture(Lcom/mojang/blaze3d/pipeline/RenderPipeline;Lnet/minecraft/util/Identifier;IIFFIIIII)V", shift = At.Shift.BEFORE), ordinal = 0)
     private int modifyVignetteColor(int color) {
+        if (!GlobalProtectionConditions.shouldProtect(MinecraftClient.getInstance().player)) return color;
         float progress = RedVignetteAction.getProgress();
         if (progress <= 0) return color;
         else {
